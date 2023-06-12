98f3ff668d55b1592ee44b0c1e14d0003ca7608b
Mostafizur-pro <mostafizur0195@gmail.com>
2023-06-12 22:14:47 +0600

    implement Zod

--- src/app/mogules/users/user.controller.ts
+++ src/app/mogules/users/user.controller.ts
@@ -1,8 +1,25 @@
 import { RequestHandler } from 'express'
 import { UserService } from './user.service'
+import { z } from 'zod'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    // req-validation
+    // body --> object
+    // data --> object
+    // note: amader schema banaite hobe object ar upor base kore
+
+    const createUserZodSchema = z.object({
+      body: z.object({
+        role: z.string({
+          required_error: 'role is required',
+        }),
+        password: z.string().optional(),
+      }),
+    })
+
+    await createUserZodSchema.parseAsync(req)
+
     const { user } = req.body
     const result = await UserService.createUser(user)
     res.status(400).json({
