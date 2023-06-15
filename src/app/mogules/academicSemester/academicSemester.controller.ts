959d1bb014679bff58b93bad62298ea1424e7c28
Mostafizur-pro <mostafizur0195@gmail.com>
2023-06-15 22:33:15 +0600

    optimize try-catch block and api response

--- src/app/mogules/academicSemester/academicSemester.controller.ts
+++ src/app/mogules/academicSemester/academicSemester.controller.ts
@@ -1,18 +1,24 @@
-import { Request, RequestHandler, Response } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { AcademicSemesterService } from './academicSemester.service'
 import catchAsync from '../../../share/catchAsync'
+import sendResponse from '../../../share/sendResponse'
+import httpStatus from 'http-status'
 
 const createSemester: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const { ...academicSemesterData } = req.body
     const result = await AcademicSemesterService.createSemester(
       academicSemesterData
     )
-    res.status(400).json({
+    next()
+    
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
-      message: 'Academic Semester is created successfully',
-      data: result,
+      message: 'Academic Semester is created successfully'
+      data: result
     })
+   
   }
 )
 
