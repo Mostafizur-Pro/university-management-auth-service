957e64cfbfdb80000cf6d4f8a327ce4018047008
Mostafizur-pro <mostafizur0195@gmail.com>
2023-06-16 12:39:10 +0600

    implement dynamic searching option

--- src/app/mogules/academicSemester/academicSemester.service.ts
+++ src/app/mogules/academicSemester/academicSemester.service.ts
@@ -20,14 +20,50 @@ const createSemester = async (
   const result = await AcademicSemester.create(payload)
   return result
 }
-// type IAcademicSemesterFilters = {
-//   searchTerm: string
-// }
 
 const getAllSemesters = async (
   filters: IAcademicSemesterFilters,
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcademicSemester[]>> => {
+  const { searchTerm } = filters
+
+  const academicSemesterSearchableFields = ['title', 'code', 'year']
+  const andConditions = []
+  if (searchTerm) {
+    andConditions.push({
+      $or: academicSemesterSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    })
+  }
+
+  // const andConditions = [
+  //   {
+  //     $or: [
+  //       {
+  //         title: {
+  //           $regex: searchTerm,
+  //           $options: 'i',
+  //         },
+  //       },
+  //       {
+  //         code: {
+  //           $regex: searchTerm,
+  //           $options: 'i',
+  //         },
+  //       },
+  //       {
+  //         year: {
+  //           $regex: searchTerm,
+  //           $options: 'i',
+  //         },
+  //       },
+  //     ],
+  //   },
+  // ]
   const { page, limit, skip, sortBy, sortOrder } =
     paginationHelpers.calculatePagination(paginationOptions)
 
@@ -35,7 +71,7 @@ const getAllSemesters = async (
   if (sortBy && sortOrder) {
     sortConditions[sortBy] = sortOrder
   }
-  const result = await AcademicSemester.find()
+  const result = await AcademicSemester.find({ $and: andConditions })
     .sort(sortConditions)
     .skip(skip)
     .limit(limit)
